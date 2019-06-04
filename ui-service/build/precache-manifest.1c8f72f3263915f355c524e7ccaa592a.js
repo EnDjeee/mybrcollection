5bf8175b6c04a29a83d76cda53fa7a29cb5a54cf
Matteo <matteo@pop-os.localdomain>
2019-06-04 16:36:24 +0200

    modify .gitignore

---
+++ ui-service/build/precache-manifest.1c8f72f3263915f355c524e7ccaa592a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "44955cf8119b6ffd098e10d594828356",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c977701ab80858775871",
+    "url": "/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "1dcf88a0cc4959736ad9",
+    "url": "/static/js/2.ff26cd06.chunk.js"
+  },
+  {
+    "revision": "c977701ab80858775871",
+    "url": "/static/js/main.334c3884.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
